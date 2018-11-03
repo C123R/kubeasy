e7f8b9b6c523471da6b9459b0566a1afd986dd8a
Cizer Pereira <cizer.ciz@gmail.com>
2018-11-03 23:09:30 +0100

    Add new version to test Azure pipeline publish test

--- kubeasy.py
+++ kubeasy.py
@@ -34,7 +34,7 @@ def print_version(ctx, param, value):
 
     if not value or ctx.resilient_parsing:
         return
-    click.echo('Kubeasy Version 0.6')
+    click.echo('Kubeasy Version 0.1.0')
     ctx.exit()
 
 
